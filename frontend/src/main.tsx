f471a167783a6036c1e5c89dabae111c69ca7c02
Dheolarh <adejuwonlookanlawon@gmail.com>
2025-11-11 17:13:22 +0100

    Production

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,28 +4,6 @@
   import App from "./App.tsx";
   import "./index.css";
 
-  // Dynamically set favicon from bundled assets so it resolves correctly in Vite/Amplify
-  import leagueFavicon from './assets/LeagueOfLegends.ico';
-
-  const setFavicon = (href: string) => {
-    try {
-      let link: HTMLLinkElement | null = document.querySelector("link[rel*='icon']");
-      if (!link) {
-        link = document.createElement('link');
-        link.rel = 'icon';
-        document.getElementsByTagName('head')[0].appendChild(link);
-      }
-      link.href = href;
-    } catch (e) {
-      // Ignore in non-browser environments
-      // eslint-disable-next-line no-console
-      console.warn('Failed to set favicon dynamically', e);
-    }
-  };
-
-  // Set favicon on load
-  setFavicon(leagueFavicon);
-
   // Remove any injected divs that might cause layout issues
   const cleanupInjectedElements = () => {
     const bodyChildren = Array.from(document.body.children);
